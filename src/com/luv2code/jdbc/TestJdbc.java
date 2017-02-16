f6d7894f9554eb17c3c3333fee11ee1cbe572f3b
Ross Brazuk <ross.brazuk@gmail.com>
2017-02-16 12:32:29 -0500

    first commit

---
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,30 @@
+package com.luv2code.jdbc;
+
+import java.sql.DriverManager;
+
+import com.mysql.jdbc.Connection;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			java.sql.Connection myConnection = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("connection successful!!!");
+			
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+
+	}
+
+}
